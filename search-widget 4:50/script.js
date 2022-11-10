4730fdf98d6c007e991b139a5a0177d27ae6b0fa
Rakhat Mamytova <67878689+RakhatMamyt@users.noreply.github.com>
2022-11-09 16:26:52 -0800

    Add files via upload

---
+++ search-widget 4:50/script.js
@@ -0,0 +1,11 @@
+const search = document.querySelector('.search');
+const button = document.querySelector('.btn');
+
+button.addEventListener('click', ()=>{
+  if(search.classList.contains('active')){
+    search.classList.remove('active')
+  } else{
+    search.classList.add('active')
+  }
+})
+ 
